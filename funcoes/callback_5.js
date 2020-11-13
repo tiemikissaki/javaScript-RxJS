a244748a884c5802014d5b5c80e6c35792daaa45
Priscilla Kissaki <tiemikissaki@gmail.com>
2020-11-13 16:16:08 -0200

    Inserir callback 5 e desafio 2 e 3 e as aulas de promise 1, 2, 3, 4 e 5

--- funcoes/callback_5.js
+++ funcoes/callback_5.js
@@ -13,3 +13,20 @@ const totalGeral = carrinho
 .map(getTotal)  
 .reduce(somar)
 console.log(totalGeral)
+
+Array.prototype.meuReduce = function(fn, inicial) {
+    let acc = inicial
+    for(let i = 0; i < this.length; i++) {
+        if(!acc && i === 0) {
+            acc = this[i]
+        } else {
+            acc = fn(acc, this[i], i, this)
+        }
+    }
+    return acc 
+}
+
+const totalGeral2 = carrinho
+.map(getTotal)  
+.meuReduce(somar)
+console.log(totalGeral2)
